04b27ad1c40d13b840d0489d8e6b2d75299c270e
chrisfranz <chrisfranz@mac.com>
2020-09-15 21:55:40 -0700

    add app folder

---
+++ app/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hi everyone')
+})
+
+app.listen(port, () => console.log(`Server listening on PORT ${port}`));
